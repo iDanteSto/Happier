f930c5d862ffd356b74728b54604319c1bc88f77
kikeg0mez <kike@ciqno.com>
2017-09-08 12:01:48 -0500

    -minor patches on conf and expired email

--- resources/views/emails/successEmail.blade.php
+++ resources/views/emails/successEmail.blade.php
@@ -4,7 +4,7 @@
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-  <title>[SUBJECT]</title>
+  <title>Email Confirmation</title>
   <style type="text/css">
 
     #outlook a {padding:0;}
