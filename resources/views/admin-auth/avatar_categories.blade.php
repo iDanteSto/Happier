3f5c0e3920b064cb818b2c17bbe37969b12fd3d0
DESKTOP-DE922TP\Kike Gomez <kike@ciqno.com>
2017-11-22 11:29:10 -0600

    -progress

--- resources/views/admin-auth/avatar_categories.blade.php
+++ resources/views/admin-auth/avatar_categories.blade.php
@@ -2,7 +2,17 @@
 
 @section('content')
 <style>
-.positioner {
+.fixposition {
+   
+
+    position: fixed;
+    top: 70%;
+    left: 50%;
+    -webkit-transform: translate(-50%, -50%);
+    transform: translate(-50%, -50%);
+    width: 60%;
+}
+.fixposition2 {
    
 
     position: fixed;
@@ -14,19 +24,19 @@
 }
 </style>
 <div class="container">
-    <div class="positioner">
+    <div class="fixposition">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Avatar Categories Section</div>
+                <div class="panel-heading">Show</div>
 
                 <div class="panel-body">
-                    <form class="form-horizontal" method="POST" action="{{ url('admin_register') }}">
-                        {{ csrf_field() }}
-
-                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
+                   
+                        
+                        
+                        <div class="col-sm-5">
                             <label for="name" class="col-md-4 control-label">Name</label>
 
-                            <div class="col-md-6">
+                            <div class="">
                                 <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
 
                                 @if ($errors->has('name'))
@@ -37,51 +47,79 @@
                             </div>
                         </div>
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+                        
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+                        
 
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
+                        <div class="col-sm-5">
+                            <label for="password-confirm" class="col-md-4 control-label">Description</label>
 
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
-
-                            <div class="col-md-6">
+                            <div class="">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
                             </div>
                         </div>
 
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
+                        <div class="col-sm-1">
+                            <div class="">
                                 <button type="submit" class="btn btn-primary">
+                                   
                                     Submit
                                 </button>
                                 
                             </div>
                         </div>
-                    </form>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="container">
+    <div class="fixposition2">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Avatar Categories Creator</div>
+
+                <div class="panel-body">
+                   
+                        
+                        
+                        <div class="col-sm-5">
+                            <label for="name" class="col-md-4 control-label">Name</label>
+
+                            <div class="">
+                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
+
+                                @if ($errors->has('name'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('name') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        
+
+                        
+
+                        <div class="col-sm-5">
+                            <label for="password-confirm" class="col-md-4 control-label">Description</label>
+
+                            <div class="">
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                            </div>
+                        </div>
+
+                        <div class="col-sm-1">
+                            <div class="">
+                                <button type="submit" class="btn btn-primary">
+
+                                    Submit
+                                </button>
+                                
+                            </div>
+                        </div>
+                    
                 </div>
             </div>
         </div>
