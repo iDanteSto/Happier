5d61f3721f6105d253bc8dbe2b64bf6f9b2f4e7d
DESKTOP-DE922TP\Kike Gomez <kike@ciqno.com>
2017-12-15 14:38:38 -0600

    -avatars

--- resources/views/admin-auth/avatars.blade.php
+++ resources/views/admin-auth/avatars.blade.php
@@ -1,9 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
-
 <style>
-.fixposition {
+.positioner {
    
 
     position: fixed;
@@ -13,53 +12,22 @@
     transform: translate(-50%, -50%);
     width: 60%;
 }
-.fixposition2 {
-   
-
-    position: fixed;
-    top: 90%;
-    left: 50%;
-    -webkit-transform: translate(-50%, -50%);
-    transform: translate(-50%, -50%);
-    width: 60%;
-}
-
-table {
-    font-family: arial, sans-serif;
-    border-collapse: collapse;
-    width: 100%;
-}
-
-td, th {
-    border: 1px solid #dddddd;
-    text-align: left;
-    padding: 8px;
-
-}
-
-tr:nth-child(even) {
-    background-color: #dddddd;
-}
 </style>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-
-
 <div class="container">
-    <div class="fixposition2">
+    <div class="positioner">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Avatars page</div>
+                <div class="panel-heading">Avatar Creator</div>
 
-<form class="form-horizontal" method="POST" action="{{ url('avatar_categ_register') }}">
                 <div class="panel-body">
-                   
-                        
-                        
-                        <div class="col-md-5">
+                    <form class="form-horizontal" method="POST" action="{{ url('admin_register') }}">
+                        {{ csrf_field() }}
+
+                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
                             <label for="name" class="col-md-4 control-label">Name</label>
 
-                            <div class="">
-                                <input id="name" type="text" class="form-horizontal" name="name" value="" required >
+                            <div class="col-md-6">
+                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
 
                                 @if ($errors->has('name'))
                                     <span class="help-block">
@@ -69,49 +37,53 @@ tr:nth-child(even) {
                             </div>
                         </div>
 
-                        
+                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                            <label for="email" class="col-md-4 control-label">Description</label>
 
-                        
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
 
-                        <div class="col-md-5">
-                            <label for="description" class="col-md-4 control-label">Description</label>
-
-                            <div class="">
-                                <input id="description" type="text" class="form-horizontal" name="description" required>
+                                @if ($errors->has('email'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
                             </div>
                         </div>
 
-                        <div class="col-md-1">
-                            
-                                <button type="submit" class="btn btn-primary" >
-                                
-                                    Submit
+                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                            <label for="password" class="col-md-4 control-label">Image</label>
+
+                            <div class="col-md-6">
+                               
+                                @if ($errors->has('password'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="password-confirm" class="col-md-4 control-label">Category</label>
+
+                            <div class="col-md-6">
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <button type="submit" class="btn btn-primary">
+                                    Register
                                 </button>
                                 
-                         
+                            </div>
                         </div>
-</div> 
-</form>                   
+                    </form>
                 </div>
             </div>
         </div>
     </div>
 </div>
-
-<script>
-
-$(document).on("click", ".Edit", function() {
-//   console.log("inside";   <-- here it is
-    console.log($(this).attr('name'));
-    window.location.href = "/avatar_categ_edit/"+$(this).attr('name');
- });
-
-
-$(document).on("click", ".Delete", function() {
-//   console.log("inside";   <-- here it is
-    console.log($(this).attr('name'));
-    window.location.href = "/avatar_categories_delete/"+$(this).attr('name');
- });
-
-</script>
 @endsection
