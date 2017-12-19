328f1dc2c8db734997d8328852e0377597dfec42
DESKTOP-DE922TP\Kike Gomez <kike@ciqno.com>
2017-12-18 23:07:05 -0600

    test2

--- resources/views/admin-auth/avatars.blade.php
+++ resources/views/admin-auth/avatars.blade.php
@@ -47,7 +47,7 @@
                             <label for="password" class="col-md-4 control-label">Image</label>
 
                             <div class="col-md-6">
-                               <input type="image" name="file">
+                               <input type="file" name="file">
                               
                             </div>
                         </div>
