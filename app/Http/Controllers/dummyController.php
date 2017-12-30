8f9e8e250ec9161c585020aeac061291092177e6
DESKTOP-DE922TP\Kike Gomez <kike@ciqno.com>
2017-12-30 12:24:03 -0600

    no message

--- app/Http/Controllers/dummyController.php
+++ app/Http/Controllers/dummyController.php
@@ -332,7 +332,6 @@ $timeOfDay =2;
 //Night
 $timeOfDay =3;
 }
-dd($timeOfDay);
 //$assign = DB::select("call recomendationSetter($userid,$timeOfDay)");
 //------------store procedure replacement------------------------------
 $choosenRecom = DB::select('SELECT DISTINCT
