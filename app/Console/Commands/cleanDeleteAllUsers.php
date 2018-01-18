79de1ca56c3afd72e951d534d97d21c3d90be8c3
DESKTOP-DE922TP\Kike Gomez <kike@ciqno.com>
2018-01-18 14:07:45 -0600

    no message

--- app/Console/Commands/cleanDeleteAllUsers.php
+++ app/Console/Commands/cleanDeleteAllUsers.php
@@ -44,15 +44,15 @@ $users = DB::table('users')->distinct()->get();
 
 foreach ($users as $usuarios) 
 {
-        DB::table('usermeditation')->where('fk_user_Id', '=', $usuarios->$user_Id)->delete();
-        DB::table('avatar_permission')->where('fk_user_Id', '=', $usuarios->$user_Id)->delete();
-        DB::table('preferred_categories')->where('fk_user_Id', '=', $usuarios->$user_Id)->delete();
-        DB::table('usermeditation')->where('fk_user_Id', '=', $usuarios->$user_Id)->delete();
-        DB::table('userfrequency')->where('fk_user_Id', '=', $usuarios->$user_Id)->delete();
-        DB::table('usermood')->where('fk_user_Id', '=', $usuarios->$user_Id)->delete();
-        DB::table('userrecommendation')->where('fk_user_Id', '=', $usuarios->$user_Id)->delete();
-        DB::table('social_provider')->where('fk_user_Id', '=', $usuarios->$user_Id)->delete();
-        DB::table('users')->where('user_Id', '=', $usuarios->$user_Id)->delete();
+        DB::table('usermeditation')->where('fk_user_Id', '=', $usuarios->user_Id)->delete();
+        DB::table('avatar_permission')->where('fk_user_Id', '=', $usuarios->user_Id)->delete();
+        DB::table('preferred_categories')->where('fk_user_Id', '=', $usuarios->user_Id)->delete();
+        DB::table('usermeditation')->where('fk_user_Id', '=', $usuarios->user_Id)->delete();
+        DB::table('userfrequency')->where('fk_user_Id', '=', $usuarios->user_Id)->delete();
+        DB::table('usermood')->where('fk_user_Id', '=', $usuarios->user_Id)->delete();
+        DB::table('userrecommendation')->where('fk_user_Id', '=', $usuarios->user_Id)->delete();
+        DB::table('social_provider')->where('fk_user_Id', '=', $usuarios->user_Id)->delete();
+        DB::table('users')->where('user_Id', '=', $usuarios->user_Id)->delete();
 }
     //$id = $this->argument('id');
 
