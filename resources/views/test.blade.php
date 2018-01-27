99c89541536f4469cc954a7ee89c2ece55432a22
DESKTOP-DE922TP\Kike Gomez <kike@ciqno.com>
2018-01-26 18:53:59 -0600

    no message

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,39 +1,64 @@
 <!doctype html>
 <html lang="{{ config('app.locale') }}">
-    <head>
+                                    <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Happier</title>
+        <title>Happier Confirmation Email</title>
 
-        <!-- Fonts -->
-        
-
-        <link href="https://fonts.googleapis.com/css?family=Fredoka+One" rel="stylesheet"> 
-        <!-- Styles -->
+<!-- links -->
+<link href="https://fonts.googleapis.com/css?family=Fredoka+One" rel="stylesheet"> 
+<!-- links -->
+<!--styles-->
 <style type="text/css">
 body
 {
 font-family: 'Fredoka One';
 }
-
-
 </style>
+<!--styles-->
+
+                                     </head>
 
 
-    </head>
-    <body>
-        
-            
 
-            <div class="content">
+
+                                     <body>
+<!--<div class="content">
                 <div class="">
                     Tester
                 </div>
+            </div>-->
+            
+<table align="center" border="0" cellpadding="1" cellspacing="0" width="757">
+<tr><!-- first segment -->
+  <td style="background-image:url(img/conf1.1.png)"  height="460" style="display: block;">
+  </td>
+</tr><!-- first segment -->
+<tr><!-- second segment -->
+  <td style="padding: 60px 0 40px 0;" align="center" bgcolor="#787878">
+    verifica
+  </td>
+</tr><!-- second segment -->
+<tr><!-- third segment -->
+  <td style="background-image:url(img/conf2.2.png)"  height="650" style="display: block;">
+  </td>
+</tr><!-- third segment -->
+<tr><!-- fourth segment -->
+  <td style="background-image:url(img/conf3.3.jpg)"  height="733" style="display: block;">
+  </td>
+</tr><!-- fourth segment -->
+<tr><!-- fifth segment -->
+  <td style="">
+    info
+  </td>
+</tr><!-- fifth segment -->
+</table>     
 
-               
-            </div>
-        </div>
-    </body>
+
+
+
+        
+                                     </body>
 </html>
