18c41852e2df601a310e96a695baab8847d63943
Dante <dante@ciqno.com>
2018-08-20 18:40:22 -0500

    moodAverageLoader set from 8 to 9.

--- app/Http/Controllers/moodController.php
+++ app/Http/Controllers/moodController.php
@@ -52,7 +52,7 @@ $weekAverages = DB::select
 'SELECT 
 avg(mood) as Average 
 FROM usermood 
-WHERE created_at >    DATE_SUB(NOW(), INTERVAL 8 WEEK) and fk_user_Id = ?
+WHERE created_at >    DATE_SUB(NOW(), INTERVAL 9 WEEK) and fk_user_Id = ?
 GROUP BY WEEK(created_at)
 ORDER BY created_at desc limit 8',[$userid]
 );
