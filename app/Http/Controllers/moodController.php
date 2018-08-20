bf1d51bb4062ccbfb029dc1faa3635f7cff4bacb
Dante <dante@ciqno.com>
2018-08-20 18:42:42 -0500

    moodAverageLoader desc limit set from 8 to 9.

--- app/Http/Controllers/moodController.php
+++ app/Http/Controllers/moodController.php
@@ -54,7 +54,7 @@ avg(mood) as Average
 FROM usermood 
 WHERE created_at >    DATE_SUB(NOW(), INTERVAL 9 WEEK) and fk_user_Id = ?
 GROUP BY WEEK(created_at)
-ORDER BY created_at desc limit 8',[$userid]
+ORDER BY created_at desc limit 9',[$userid]
 );
 if($weekAverages)
 {
